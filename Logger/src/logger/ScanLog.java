a8092e6d544493155d33d3195119dd1c1e5059f8
Tiago <Tiago@192.168.1.101>
2012-03-11 18:15:32 +0000

    Limpar coisas e faz print do tempo que leva a processar

--- Logger/src/logger/ScanLog.java
+++ Logger/src/logger/ScanLog.java
@@ -37,20 +37,25 @@ public class ScanLog implements Runnable {
     private static long totalService = 0;
     private boolean t = false;
     private static int i = 0;
-    private String registo = "";
+    private static String registo = "";
     private final String start = "<host";
     private final String end = "</host>";
     private final String open = "open";
+    private final String filtered = "filtered";
     private final String service = "hostname=\"";
     private final String product = "product=\"";
     private final String address = "addr=\"";
     private int totalRegistos = 0;
     private int totalOpen = 0;
+    private static int x = -1;
+    private static String s = "";
+    private static Integer n = null;
     //0 -> normal
     //1 -> procurar por stirng
     private static int searchMode = 0;
-    BNDM b = new BNDM();
-    StringSearch so = new BoyerMooreHorspoolRaita();
+    private static BNDM b = new BNDM();
+    private static StringSearch so = new BoyerMooreHorspoolRaita();
+    private static long time = 0;
 
     /**
      * @param args the command line arguments
@@ -78,45 +83,31 @@ public class ScanLog implements Runnable {
         }
     }
 
-    private static String getTagValue(String sTag, Element eElement) {
-        NodeList nlList = eElement.getElementsByTagName(sTag).item(0).getChildNodes();
-
-        Node nValue = (Node) nlList.item(0);
-
-        return nValue.getNodeValue();
-    }
-
     public void run() {
 
         try {
-
-            int i = 0;
-            long total = 0;
+            time = System.currentTimeMillis();
             System.out.println("Start processing -> " + filename);
             BufferedReader br = new BufferedReader(new FileReader(filename));
             String line;
             String serviceFound;
             String productFound;
-            String addressFound = "";
             int x = -1;
 
             //Processar ficheiro..
             while ((line = br.readLine()) != null) {
                 if (line.length() > 0) {
                     //Procurar por um host
-                    x = so.searchChars(line.toCharArray(), start.toCharArray());
-                    if (x != -1) {
+                    if (so.searchChars(line.toCharArray(), start.toCharArray()) != -1) {
+                        totalRegistos++;
                         registo = line;
                         //Enquanto não encontrar o fim adiciona
                         while ((line = br.readLine()) != null) {
                             registo += line;
-                            x = so.searchChars(line.toCharArray(), end.toCharArray());
-                            if (x != -1) {
-                                totalRegistos++;
+                            if (so.searchChars(line.toCharArray(), end.toCharArray()) != -1) {
                                 //Encontrar host abert
-                                x = so.searchChars(registo.toCharArray(), open.toCharArray());
-                                if (x != -1) {
-                                    System.out.println(registo);
+                                if (so.searchChars(registo.toCharArray(), open.toCharArray()) != -1) {
+//                                    System.out.println(registo);
                                     //Encontrar product name
                                     productFound = getAttribute(registo, product);
                                     addProduct(productFound);
@@ -126,20 +117,20 @@ public class ScanLog implements Runnable {
                                     //Encontrar service
                                     serviceFound = getAttribute(registo, service);
                                     addService(serviceFound);
-
                                     totalOpen++;
                                     break;
                                 }
-                                registo = "";
                                 break;
                             } else {
                                 registo += line;
+                            }
+                            if (so.searchChars(line.toCharArray(), filtered.toCharArray()) != -1) {
+                                break;
                             }
                         }
                     }
                 }
             }
-
             br.close();
         } catch (Exception e) {
             System.err.println("File input error");
@@ -163,12 +154,14 @@ public class ScanLog implements Runnable {
         }
         System.out.println("Total Versoes - " + totalProduct);
         System.out.println("###################################");
-        
+
         System.out.println("\n\n");
         System.out.println("Total registos -> " + totalRegistos);
         System.out.println("Total Open -> " + totalOpen);
 
         System.out.println("\n\n");
+
+        System.out.println("Done in " + (System.currentTimeMillis() - time) + " ms");
     }
 
     private void addProduct(String productFound) {
@@ -176,13 +169,13 @@ public class ScanLog implements Runnable {
             if (productFound.isEmpty()) {
                 productFound = "UNKOWN";
             }
-            Long n = (Long) productList.get(productFound);
+            n = (Integer) productList.get(productFound);
             totalProduct++;
             if (n != null) {
-                n = n + 1;
+                n++;
                 productList.put(productFound, n);
             } else {
-                productList.put(productFound, 1L);
+                productList.put(productFound, 1);
             }
         }
     }
@@ -192,19 +185,19 @@ public class ScanLog implements Runnable {
             if (serviceFound.isEmpty()) {
                 serviceFound = "UNKOWN";
             }
-            Long n = (Long) serviceList.get(serviceFound);
+            n = (Integer) serviceList.get(serviceFound);
             totalService++;
             if (n != null) {
-                n = n + 1;
+                n++;
                 serviceList.put(serviceFound, n);
             } else {
-                serviceList.put(serviceFound, 1L);
+                serviceList.put(serviceFound, 1);
             }
         }
     }
 
     private String getAttribute(String line, String attribute) {
-        String s = "";
+
         int x = so.searchChars(line.toCharArray(), attribute.toCharArray());
         if (x != -1) {
             String subString = registo.substring(attribute.length() + x, registo.length());
